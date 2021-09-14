8eb5629e502dd34d870bc6f42abd1cd2ef01d87b
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-13 21:40:29 -0400

    server up

--- server/index.js
+++ server/index.js
@@ -3,8 +3,25 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://KevinrToday:krtoday@nomadee.bsw8d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server runnion on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify, false");
---
+++ server/routes/posts.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const router = express.Router();
+
+//localhost:5000/posts
+
+router.get("/", (req, res) => {
+  res.send("THIS WORKS");
+});
+
+export default router;
