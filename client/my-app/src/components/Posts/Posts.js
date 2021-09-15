beb887bad28cbe05d2ff6803957392d538a21f7f
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-14 23:16:53 -0400

    api dispatch

--- client/my-app/src/components/Posts/Posts.js
+++ client/my-app/src/components/Posts/Posts.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Post from "./Post/Post";
+import useStyles from "./styles";
 
 const Posts = () => {
+  const classes = useStyles();
   return (
     <>
-      <h1> POSTS </h1>
+      <h1 className={classes.posts}> POSTS </h1>
       <Post />
       <Post />
     </>
